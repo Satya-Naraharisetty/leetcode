b3f089c162716e99f8c37429a5b2a953361d3172
20PA1A04B1 <82475753+siddu369@users.noreply.github.com>
2023-07-03 21:31:31 +0530

    Create Chocolate Distribution Problem.java

---
+++ Chocolate Distribution Problem.java
@@ -0,0 +1,16 @@
+class Solution {
+    static int findMinDiff(int arr[], int n, int m){
+        if (m == 0 || n == 0){
+            return 0;
+        }
+        Arrays.sort(arr);
+        int min_diff = Integer.MAX_VALUE;
+        for (int i = 0; i + m - 1 < n; i++){
+            int diff = arr[i + m - 1] - arr[i];
+            if (diff < min_diff){
+                min_diff = diff;
+            }
+        }
+        return min_diff;
+    }
+}
