caca765f9ec59436082cd9addc069b077f0b8de4
20PA1A04B1 <82475753+siddu369@users.noreply.github.com>
2023-07-04 23:23:41 +0530

    Update Two Sum.java

--- Two Sum.java
+++ Two Sum.java
@@ -10,3 +10,21 @@ class Solution {
         return new int[] {};
     }
 }
+
+Time Complexity: O(n^2)
+
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (hm.containsKey(target - nums[i])) {
+                return new int[] {hm.get(target - nums[i]), i};
+            }
+            hm.put(nums[i], i);
+        }
+        return new int[] {};
+    }
+}
+
+Time Complexity: O(n)
